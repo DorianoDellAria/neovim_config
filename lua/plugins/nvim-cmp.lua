53d24ff87fb0fc6d1756b43860207f8084a49d3c
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2023-12-28 22:57:27 +0100

    feat(cmp): add sources

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,11 +1,46 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local kind_icons = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
 -- Autocompletion
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
 
+    "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
 
     "rafamadriz/friendly-snippets",
   },
@@ -32,27 +67,47 @@ return {
           select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-          -- if cmp.visible() then
-          --   cmp.select_next_item()
-          if luasnip.expand_or_locally_jumpable() then
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          -- if cmp.visible() then
-          --   cmp.select_prev_item()
-          if luasnip.locally_jumpable(-1) then
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
         end, { "i", "s" }),
       }),
+      formatting = {
+        fields = { "menu", "abbr", "kind" },
+        format = function(entry, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+          -- Source
+          vim_item.menu = ({
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+        expandable_indicator = true,
+      },
       sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
       },
     })
   end,
