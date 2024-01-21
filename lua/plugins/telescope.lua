e12db14358e8d4545269a67d240e1e87f918b150
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2024-01-21 17:12:19 +0100

    feat(plugin): add telescope find hidden file function

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -68,13 +68,19 @@ return {
       end
     end
 
-    local function find_files_git_root()
+    local function find_files_git_root(hidden_param)
       local git_root = find_git_root()
+      local hidden = hidden_param or false
       if git_root then
         require("telescope.builtin").find_files({
           cwd = git_root,
+          hidden = hidden,
         })
       end
+    end
+
+    local function find_hidden_files_git_root()
+      find_files_git_root(true)
     end
 
     vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
@@ -93,6 +99,7 @@ return {
 
     vim.keymap.set("n", "<leader>gf", telescope_builtin.git_files, { desc = "Search [G]it [F]iles" })
     vim.keymap.set("n", "<leader>f", find_files_git_root, { desc = "Search [F]iles" })
+    vim.keymap.set("n", "<leader>F", find_hidden_files_git_root, { desc = "Search hidden [F]iles" })
     vim.keymap.set("n", "<leader>sh", telescope_builtin.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sw", telescope_builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader><space>", live_grep_git_root, { desc = "[S]earch by [G]rep in git root or cwd" })
