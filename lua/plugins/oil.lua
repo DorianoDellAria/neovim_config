85ba3643e002f8eefda78e87d8bedae4e1ab0fec
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2024-02-02 09:32:15 +0100

    feat(plugin): oil remove unsused keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function ()
-    require("oil").setup()
+    require("oil").setup({
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+      },
+    })
     vim.keymap.set("n", "-", ":Oil<cr>", { desc = "Open oil" } )
   end,
 }
