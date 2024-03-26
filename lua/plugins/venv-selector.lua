1654976b29d923d20b2ec32574f9fa6b36f2a971
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2024-03-26 23:10:32 +0100

    feat(plugin): add venv selector

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,19 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  config = function()
+    local git_root = require("doriconfig.utils").find_git_root()
+    require("venv-selector").setup({
+      name = ".venv",
+      path = git_root,
+      parents = 0,
+    })
+  end,
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+  },
+}
