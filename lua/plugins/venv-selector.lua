e604bd67d26a616b7e69beadac1cfeb3e752251c
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2024-06-24 22:33:15 +0200

    chore(venv-selector): remove old params and keybinds

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -3,15 +3,8 @@ return {
   branch = "regexp",
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   config = function()
-    local git_root = require("doriconfig.utils").find_git_root()
-    require("venv-selector").setup({
-      name = ".venv",
-      path = git_root,
-      parents = 0,
-    })
+    require("venv-selector").setup({})
 
     vim.keymap.set("n", "<leader>vs", ":VenvSelect<cr>", { desc = "Select venv"})
-    vim.keymap.set("n", "<leader>vc", ":VenvSelectCached<cr>", { desc = "Activate cached venv"})
   end,
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 }
