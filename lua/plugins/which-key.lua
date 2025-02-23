71ddc8b4f1d58857cc29e969c814ea4a1529e0d5
Doriano Dell'Aria <doriano.dellaria@necko.tech>
2025-02-23 20:20:15 +0100

    mini.icons

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,6 +1,7 @@
 -- Plugin that show pending keybinds
 return {
   "folke/which-key.nvim",
+  dependencies = { "echasnovski/mini.icons" },
   config = function()
     -- document existing key chains
     require("which-key").add({
